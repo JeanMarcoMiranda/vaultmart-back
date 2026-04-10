dafd0f558f2ce787e6e81a0bef015db69f9f574a
JeanMarcoMiranda <elecord2014@gmail.com>
2026-04-10 01:54:44 -0500

    feat: adding create and findByEmail to UsersService

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(userData: Partial<User> & { password: string }): Promise<User> {
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const user = this.userRepository.create({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    return await this.userRepository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.userRepository.findOne({
+      where: { email },
+      select: ['id', 'email', 'password', 'role', 'tenantId'],
+    });
+  }
+}
