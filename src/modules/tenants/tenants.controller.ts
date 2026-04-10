2561ffeaba4ba710ace42c951b33ec0ead6e8a64
JeanMarcoMiranda <elecord2014@gmail.com>
2026-04-09 20:07:19 -0500

    feat: adding tenant contoller function to get all tenants and create a new tenant

--- src/modules/tenants/tenants.controller.ts
+++ src/modules/tenants/tenants.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TenantsService } from './tenants.service';
 
 @Controller('tenants')
-export class TenantsController {}
+export class TenantsController {
+  constructor(private readonly tenantService: TenantsService) {}
+
+  @Post()
+  create(@Body() body: { name: string; slug: string }) {
+    return this.tenantService.create(body.name, body.slug);
+  }
+
+  @Get()
+  findAll() {
+    return this.tenantService.findAll();
+  }
+}
