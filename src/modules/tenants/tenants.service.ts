f16e8bb02dc0b6001d69d1522fbc38c94010f760
JeanMarcoMiranda <elecord2014@gmail.com>
2026-04-09 20:04:37 -0500

    feat: adding tenant service functions for bussiness logic

--- src/modules/tenants/tenants.service.ts
+++ src/modules/tenants/tenants.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Tenant } from './entities/tenant.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TenantsService {}
+export class TenantsService {
+  constructor(
+    @InjectRepository(Tenant)
+    private readonly tenantRepository: Repository<Tenant>,
+  ) {}
+
+  async create(name: string, slug: string): Promise<Tenant> {
+    const existing = await this.tenantRepository.findOne({ where: { slug } });
+
+    if (existing) {
+      throw new ConflictException('Tenant with this slug already exists');
+    }
+
+    const tenant = this.tenantRepository.create({ name, slug });
+    return await this.tenantRepository.save(tenant);
+  }
+
+  async findAll(): Promise<Tenant[]> {
+    return await this.tenantRepository.find();
+  }
+}
